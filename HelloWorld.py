adfdbf555e9b73d12e95477947c627af770fa472
AryanPatel111 <155606446+AryanPatel111@users.noreply.github.com>
2024-01-31 23:34:30 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 # This program will print Hello World
-print("Hello World...again n again n again\n");
+print("Hello World...again n again n again\n")
+print("hiiiiiiiii")
 
 
 
